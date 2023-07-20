49f39e32db54f39f21a4e3ea67fbf646ff776142
Emil Gainullin <e.gainullin@innopolis.university>
2023-07-20 19:11:32 +0300

    Basics of DirectX12

--- src/world/camera.cpp
+++ src/world/camera.cpp
@@ -91,7 +91,6 @@ const DirectX::XMMATRIX cg::world::camera::get_dxm_view_matrix() const
 	DirectX::FXMVECTOR eye_direction{get_direction().x, get_direction().y, get_direction().z};
 	DirectX::FXMVECTOR up_direction{get_up().x, get_up().y, get_up().z};
 	return DirectX::XMMatrixLookToRH(eye_position, eye_direction, up_direction);
-
 }
 
 const DirectX::XMMATRIX cg::world::camera::get_dxm_projection_matrix() const
